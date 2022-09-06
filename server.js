1d6cb3a45367067d1888bf88fd71ab35f03e5afe
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-09-06 15:47:37 -0400

    Initial commit test. Added comments and documentation in server.js.

--- server.js
+++ server.js
@@ -1,48 +1,62 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
-const logger = require('morgan')
-const connectDB = require('./config/database')
-const mainRoutes = require('./routes/main')
-const asciiRoutes = require('./routes/asciis')
+// Express Application. Require other Node Modules: mongoose, nodemon, passport, and morgan
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('express-flash');
+const logger = require('morgan');
+const MongoStore = require('connect-mongo')(session);
+const connectDB = require('./config/database');
+const mainRoutes = require('./routes/main');
+const asciiRoutes = require('./routes/asciis');
 
+// set path for .env file:
+require('dotenv').config( {path: './config/.env'} );
 
-require('dotenv').config({path: './config/.env'})
+// passport config:
+require('./config/passport')(passport);
 
-// Passport config
-require('./config/passport')(passport)
+// connect to the MongoDB:
+// [Ensure that the DB_STRING in .env file is set to the proper MongoDB connect. Ensure that it is of the type string.]
+connectDB();
 
-connectDB()
+// set view structure as EJS, and enable reading and writing of JSON
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger('dev'));
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(logger('dev'))
-// Sessions
+// sessions
 app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    })
-  )
-  
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: false,
+		store: new MongoStore({ mongooseConnection: mongoose.connection }),
+	})
+)
+	
 // Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(flash())
-  
-app.use('/', mainRoutes)
-app.use('/asciis', asciiRoutes)
-app.post('/upload', asciiRoutes)
+app.use(flash());
+
+// routing to the main landing page, and the ASCII page once logged in
+app.use('/', mainRoutes);
+app.use('/asciis', asciiRoutes);
  
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running, you better catch it!')
-})    
+app.listen(process.env.PORT, () => {
+	console.log("Starting server...");
+	console.log("Server is running...");
+})
+
+/*
+Full Stack structure:
+	- This file, server.js is the main file. This is the entry point to the backend.
+	- All API requests go through the router in the /routes/ folder.
+	- General path:
+		- (This file) server.js --> routes --> controllers --> views --> render page and display to user.
+*/
